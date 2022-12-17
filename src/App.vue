9f4e0c387a68d8066b122e7db97f178db2715575
Dias <wprogrammist@gmail.com>
2022-12-17 16:54:19 +0600

    добавил анимацию для скелетона

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,6 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
 
 export default defineComponent({
   name: 'App',
@@ -91,6 +90,30 @@ html {
   }}.sceleton{
     background: linear-gradient(90deg, #3C3C3C 0%, #444444 51.04%, #333333 100%);
 
+    position: relative;
+    overflow: hidden;
+    &::after {
+      position: absolute;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      transform: translateX(-100%);
+      background-image: linear-gradient(
+        90deg,
+        rgba(#fff, 0) 0,
+        rgba(#fff, 0.2) 20%,
+        rgba(#fff, 0.5) 60%,
+        rgba(#fff, 0)
+      );
+      animation: shimmer 3s infinite linear;
+      content: '';
+    }
+    @keyframes shimmer {
+      100% {
+        transform: translateX(100%);
+      }
+    }
   }.screen{&__footer{}}
 
 
